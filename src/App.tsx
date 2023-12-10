e81f82b573d18f50c50dfeed1492c84ea93a76a5
Emilly Soares Santos <soaressantosemilly05@gmail.com>
2023-12-10 18:42:03 -0400

    Adding the interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,23 @@
+import { useEffect, useState, useCallback, useMemo, useRef, ChangeEvent } from "react";
 import './App.css'
+
+interface EditMovie {
+  enabled: boolean;
+  movieTitle: string;
+}
+
+interface Movie {
+  id: number;
+  title: string;
+  poster: string;
+  synopsis: string;
+  cast: string;
+  rating: number;
+  category: string;
+  premiere: string;
+  duration: number;
+}
+
 
 function App() {
 
