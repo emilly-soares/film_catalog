6926e214c6150c5c53c4134f9e9655155a8917d5
Emilly Soares Santos <soaressantosemilly05@gmail.com>
2023-12-12 19:32:14 -0400

    adding home button

--- src/App.tsx
+++ src/App.tsx
@@ -19,10 +19,24 @@ interface Movie {
   duration: number;
 }
 
-
 export default function App() {
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrolledDown = window.scrollY > 100; // Altere o valor conforme necessário
+      document.body.classList.toggle("scrolled-down", scrolledDown);
+    };
+  
+    window.addEventListener("scroll", handleScroll);
+  
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  
+
   const inputRef = useRef<HTMLInputElement>(null);
+  const formRef = useRef<HTMLDivElement>(null);
   const firstRender = useRef(true);
   const [input, setInput] = useState<Movie>({
     title: "",
@@ -34,6 +48,7 @@ export default function App() {
     premiere: "",
     duration: 0,
   });
+
   const [movies, setMovies] = useState<Movie[]>([]);
   const [editMovie, setEditMovie] = useState<EditMovie>({
     enabled: false,
@@ -91,12 +106,27 @@ export default function App() {
     setMovies(removeMovie);
   }, [movies]);
 
+  const scrollToTop = () => {
+    if (formRef.current) {
+      formRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  };
+
   const handleEdit = useCallback((item: Movie) => {
     setInput(item);
     setEditMovie({
       enabled: true,
       movieTitle: item.title,
     });
+    if (formRef.current) {
+      formRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
   }, []);
 
   const handleRegister = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
@@ -134,91 +164,94 @@ export default function App() {
   return (
 
     <div className="container">
+      <div className="form" ref={formRef}>
+        <h1>Catálogo de Filmes</h1>
 
-      <h1>Lista de Filmes</h1>
-      <label>Título do Filme:</label>
-      <input
-        type="text"
-        placeholder="Digite o título do filme aqui"
-        value={input.title}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => setInput({ ...input, title: e.target.value })}
-        ref={inputRef}
-      />
+        <label>Título do Filme:</label>
+        <input
+          type="text"
+          placeholder="Digite o título do filme aqui"
+          value={input.title}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setInput({ ...input, title: e.target.value })}
+          ref={inputRef}
+        />
 
-      <label>URL do Pôster:</label>
-      <input
-        type="text"
-        placeholder="Digite a URL do pôster aqui"
-        value={input.poster}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => setInput({ ...input, poster: e.target.value })}
-      />
+        <label>URL do Pôster:</label>
+        <input
+          type="text"
+          placeholder="Digite a URL do pôster aqui"
+          value={input.poster}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setInput({ ...input, poster: e.target.value })}
+        />
 
-      <label>Sinopse:</label>
-      <textarea
-        placeholder="Digite a sinopse do filme aqui"
-        value={input.synopsis}
-        onChange={(e: ChangeEvent<HTMLTextAreaElement>) => setInput({ ...input, synopsis: e.target.value })}
-      />
+        <label>Sinopse:</label>
+        <textarea
+          placeholder="Digite a sinopse do filme aqui"
+          value={input.synopsis}
+          onChange={(e: ChangeEvent<HTMLTextAreaElement>) => setInput({ ...input, synopsis: e.target.value })}
+        />
 
-      <label>Elenco:</label>
-      <input
-        type="text"
-        placeholder="Digite o elenco do filme aqui"
-        value={input.cast}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => setInput({ ...input, cast: e.target.value })}
-      />
+        <label>Elenco:</label>
+        <input
+          type="text"
+          placeholder="Digite o elenco do filme aqui"
+          value={input.cast}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setInput({ ...input, cast: e.target.value })}
+        />
 
-      <label>Avaliação:</label>
-      <input
-        type="number"
-        placeholder="Digite a avaliação do filme aqui"
-        value={input.rating}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => setInput({ ...input, rating: parseFloat(e.target.value) })}
-      />
+        <label>Avaliação:</label>
+        <input
+          type="number"
+          placeholder="Digite a avaliação do filme aqui"
+          value={input.rating}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setInput({ ...input, rating: parseFloat(e.target.value) })}
+        />
 
-      <label>Categoria:</label>
-      <input
-        type="text"
-        placeholder="Digite a categoria do filme aqui"
-        value={input.category}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => setInput({ ...input, category: e.target.value })}
-      />
+        <label>Categoria:</label>
+        <input
+          type="text"
+          placeholder="Digite a categoria do filme aqui"
+          value={input.category}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setInput({ ...input, category: e.target.value })}
+        />
 
-      <label>Estreia:</label>
-      <input
-        type="text"
-        placeholder="Digite a data de estreia do filme aqui"
-        value={input.premiere}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => setInput({ ...input, premiere: e.target.value })}
-      />
+        <label>Estreia:</label>
+        <input
+          type="text"
+          placeholder="Digite a data de estreia do filme aqui"
+          value={input.premiere}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setInput({ ...input, premiere: e.target.value })}
+        />
 
-      <label>Duração (minutos):</label>
-      <input
-        type="number"
-        placeholder="Digite a duração do filme em minutos"
-        value={input.duration}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => setInput({ ...input, duration: parseInt(e.target.value) })}
-      />
+        <label>Duração (minutos):</label>
+        <input
+          type="number"
+          placeholder="Digite a duração do filme em minutos"
+          value={input.duration}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => setInput({ ...input, duration: parseInt(e.target.value) })}
+        />
+        <button onClick={handleRegister}>
+          {editMovie.enabled ? "Alterar" : "Adicionar"}
+        </button>
 
-      <button onClick={handleRegister}>
-        {editMovie.enabled ? "Alterar" : "Adicionar"}
-      </button>
+      </div>
 
       <div className="card-container">
 
         {movies.map((item, index) => (
           <section key={index} className="card">
             <h3>{item.title}</h3>
-            <img src={item.poster} alt={item.title} />
+            <div className="poster" onClick={() => handleDetails(item)}>
+              <img src={item.poster} alt={item.title} />
+            </div>
             <div className="button-container">
-
-              <button onClick={() => handleDelete(item)}>
+              <button className="btn" onClick={() => handleDelete(item)}>
                 <FontAwesomeIcon icon={faTrash} />
               </button>
-              <button onClick={() => handleEdit(item)}>
+              <button className="btn" onClick={() => handleEdit(item)}>
                 <FontAwesomeIcon icon={faEdit} />
               </button>
-              <button onClick={() => handleDetails(item)}>
+              <button className="btn" onClick={() => handleDetails(item)}>
                 <FontAwesomeIcon icon={faPlus} />
               </button>
             </div>
@@ -242,7 +275,9 @@ export default function App() {
           </div>
         </div>
       )}
-
+      <button className="scroll-to-top" onClick={scrollToTop}>
+        &#8593;
+      </button>
     </div>
   );
 }
